3eefb92da53a21868932df158308e4f280bc925d
RaddiZeCat <maxraddatz98@gmail.com>
2024-06-28 13:52:44 +0200

    little cange to bubble

--- Code/bubble.gd
+++ Code/bubble.gd
@@ -5,10 +5,11 @@ var time:int
 
 
 func _ready():
+	time = rng.randf_range(0,4)
+	await get_tree().create_timer(time).timeout
 	$AnimationPlayer.play("blubb")
 
 func _on_animation_player_animation_finished(anim_name):
 	time = rng.randf_range(3,7)
-	print(time)
 	await get_tree().create_timer(time).timeout
 	$AnimationPlayer.play("blubb")
